566e50efc69846d4aa12ba03752d60c74c44b92d
ChowdhuryJoyanti <joyantichowdhury7070@gmail.com>
2025-08-25 19:39:05 +0600

    base api added

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -4,9 +4,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+  // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
 // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
-// baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-system-two-tawny.vercel.app/api' }),
+baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-system-two-tawny.vercel.app/api' }),
   tagTypes: ['book', 'borrow'],
   endpoints: (builder) => ({
     getAllBooks: builder.query({
